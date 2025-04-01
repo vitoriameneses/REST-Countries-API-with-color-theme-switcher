6e2483251da0984fd34b8e0c4292912a21c0d478
Vitória Teixeira <vitoria.menesesmt@gmail.com>
2025-04-01 11:19:07 -0300

    add angular material

--- countries-api-color-switcher/src/app/app.component.ts
+++ countries-api-color-switcher/src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HttpClient } from '@angular/common/http';
+import { response } from 'express';
+import { error } from 'console';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    MatToolbarModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'countries-api-color-switcher';
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    const apiUrl = 'https://restcountries.com/v3.1/all?fields=name,flags';
+    this.http.get(apiUrl).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log('erro: ',error);
+      }
+    );
+  }
 }
